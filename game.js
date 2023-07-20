c89d3d016a52e64bfa16535fb1db27d9d04ada13
RudyDupuis <abcdupuisrudy@gmail.com>
2023-07-20 15:23:05 +0200

    Finish

--- game.js
+++ game.js
@@ -3,6 +3,12 @@ import { addRandomValue } from "./game/AddRandomValue.js";
 import { tilesCreation } from "./game/TilesCreation.js";
 import { tilesMoving } from "./game/TilesMoving.js";
 import { scoreCalculation } from "./ia/ScoreCalculation.js";
+import { createTestGameBoard } from "./ia/CreateTestGameBoard.js";
+
+const UP = 0;
+const BOTTOM = 1;
+const LEFT = 2;
+const RIGHT = 3;
 
 let gameBoardValues = [
   ["0", "0", "0", "0"],
@@ -24,48 +30,32 @@ async function runLoopWithDelay() {
   do {
     let isMoving;
 
-    if (round % 2 == 0) {
-      isMoving = tilesMoving(gameBoardValues, 0);
+    let gameBoardTest = createTestGameBoard(gameBoardValues);
+
+    if (tilesMoving(gameBoardTest, UP)) {
+      isMoving = tilesMoving(gameBoardValues, UP);
+    } else if (tilesMoving(gameBoardTest, LEFT)) {
+      isMoving = tilesMoving(gameBoardValues, LEFT);
+    } else if (tilesMoving(gameBoardTest, RIGHT)) {
+      isMoving = tilesMoving(gameBoardValues, RIGHT);
+    } else if (tilesMoving(gameBoardTest, BOTTOM)) {
+      isMoving = tilesMoving(gameBoardValues, BOTTOM);
     } else {
-      isMoving = tilesMoving(gameBoardValues, 2);
+      run = false;
     }
 
     if (isMoving) {
       addRandomValue(gameBoardValues);
-    } else {
-      if (round % 2 == 0) {
-        isMoving = tilesMoving(gameBoardValues, 2);
-      } else {
-        isMoving = tilesMoving(gameBoardValues, 0);
-      }
-
-      if (isMoving) {
-        addRandomValue(gameBoardValues);
-      } else {
-        isMoving = tilesMoving(gameBoardValues, 1);
-
-        if (isMoving) {
-          addRandomValue(gameBoardValues);
-        } else {
-          isMoving = tilesMoving(gameBoardValues, 3);
-
-          if (isMoving) {
-            addRandomValue(gameBoardValues);
-          } else {
-            run = false;
-          }
-        }
-      }
     }
 
     tilesCreation(gameBoardValues);
     round++;
 
-    let score = scoreCalculation(gameBoardValues);
     document.querySelector(".round").textContent = round;
-    document.querySelector(".score").textContent = score;
+    document.querySelector(".score").textContent =
+      scoreCalculation(gameBoardValues);
 
-    await new Promise((resolve) => setTimeout(resolve, 50));
+    await new Promise((resolve) => setTimeout(resolve, 1));
   } while (run);
 }
 
